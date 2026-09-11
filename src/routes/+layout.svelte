434c67b3554e22d816508d4c711aae066239b220
Caitlin Donahue <CaitlinDonahue95@gmail.com>
2026-09-11 18:10:28 -0500

    prep for ghpages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,35 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import { onNavigate } from '$app/navigation';
-	import '$lib/styles/media-queries.css';
-	import '$lib/styles/resets.css';
-	import '$lib/styles/utilities.css';
-	import '$lib/styles/colors.css';
-	import '$lib/styles/variables.css';
-	import '$lib/styles/typography.css';
-	import '$lib/styles/globals.css';
+  import favicon from '$lib/assets/favicon.svg';
+  import { onNavigate } from '$app/navigation';
+  import '$lib/styles/media-queries.css';
+  import '$lib/styles/resets.css';
+  import '$lib/styles/utilities.css';
+  import '$lib/styles/colors.css';
+  import '$lib/styles/variables.css';
+  import '$lib/styles/typography.css';
+  import '$lib/styles/globals.css';
 
-	onNavigate((navigation) => {
-		if (!document.startViewTransition) return;
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
 
-		return new Promise((resolve) => {
-			document.startViewTransition(async () => {
-				resolve();
-				await navigation.complete;
-			});
-		});
-	});
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 
-	let { children } = $props();
+  let { children } = $props();
+
+  // eslint-disable-next-line svelte/valid-prop-names-in-kit-pages
+  export const prerender = true;
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
 <div class="page-container">
-	{@render children()}
+  {@render children()}
 </div>
